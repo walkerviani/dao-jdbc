d9a20968b64d8fcd78fb0cbb255ef12cb1e76960
Walker Viani <walkeryslan@gmail.com>
2025-12-12 18:53:42 -0300

    implementando findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,11 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		System.out.println("=== TESTE 3: seller findAll ===");
+	    list = sellerDao.findAll();
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
 				
 	}
 
